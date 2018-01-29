5bf744b1e6baffc3c70f429bfbd7ef7dcd50bf71
Aimad <aimad.takhtoukh@gmail.com>
2018-01-29 01:54:18 +0100

    Changed the parameters to env.properties.

--- restart.sh
+++ restart.sh
@@ -3,4 +3,4 @@ mvn clean package
 docker stop filerepo
 docker rm filerepo
 docker build -t filerepo .
-docker run -d --name filerepo --env-file ./env.list -p 80:80 -v --restart=always filerepo
+docker run -d --name filerepo --env-file ./env.properties -p 80:80 -v --restart=always filerepo
