a82a9e8087e017cf5c227bf20163f2b8614b9939
Aimad <aimad.takhtoukh@gmail.com>
2018-02-21 20:29:06 +0100

    Update shells

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 export $(cat ./database/env.properties | xargs)
 export $(cat ./back/env.properties | xargs)
+mvn clean install -U -DskipTests
+eval $(docker-machine env)
 docker-compose build
--- restart.sh
+++ restart.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+export $(cat ./database/env.properties | xargs)
+export $(cat ./back/env.properties | xargs)
+eval $(docker-machine env)
 docker-compose down
 docker-compose up
