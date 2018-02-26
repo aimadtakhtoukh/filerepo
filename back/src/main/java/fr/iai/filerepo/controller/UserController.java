957e270f6455cde68113ca111aabf37cb65ec935
Aimad <aimad.takhtoukh@gmail.com>
2018-02-27 00:13:37 +0100

    POST /user/update/{username} throws an error if you're not an admin and edit someone else's account

--- back/src/main/java/fr/iai/filerepo/controller/UserController.java
+++ back/src/main/java/fr/iai/filerepo/controller/UserController.java
@@ -3,20 +3,29 @@ package fr.iai.filerepo.controller;
 import fr.iai.filerepo.database.RoleRepository;
 import fr.iai.filerepo.database.UserRepository;
 import fr.iai.filerepo.domain.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.keygen.KeyGenerators;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/user/")
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
@@ -52,12 +61,22 @@ public class UserController {
     }
 
     @PostMapping("update/{username}")
+    @PreAuthorize("hasAnyAuthority('ADMIN_USER', 'STANDARD_USER')")
     public void updateUserAccount(@PathVariable String username,
                                   @RequestParam(required = false) String login,
                                   @RequestParam(required = false) String password,
                                   @RequestParam(required = false) String firstName,
-                                  @RequestParam(required = false) String lastName) {
+                                  @RequestParam(required = false) String lastName,
+                                  Authentication a) {
         User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new IllegalArgumentException("User does not exist");
+        }
+        if (a.getAuthorities().stream().noneMatch(authority -> authority.getAuthority().equals("ADMIN_USER"))) {
+            if (!Objects.equals(a.getName(), username)) {
+                throw new IllegalArgumentException(String.format("Non-admin user %s is trying to edit the account %s", a.getName(), username));
+            }
+        }
         if (!StringUtils.isEmpty(firstName)) {
             user.setFirstName(firstName);
         }
@@ -81,6 +100,14 @@ public class UserController {
         User activatingUser = userRepository.findByUsername(username);
         activatingUser.setEnabled(true);
         userRepository.save(activatingUser);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity handleIOException(IllegalArgumentException e) {
+        logger.error("Erreur de connexion", e);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("{\"error\" : \"Erreur de connexion\"}");
     }
 
 
